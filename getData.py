dd8a250ebea4120af487182b02b44e76cf099211
SrStiv <estivenpc312@gmail.com>
2025-10-23 20:13:44 -0500

    conexion

---
+++ getData.py
@@ -0,0 +1,19 @@
+#script python
+import serial
+import time
+import os 
+
+arduino_port ='comx'
+band_rate = 9600
+ser = serial.Serial(arduino_port, band_rate,timeout=1)
+time.sleep(2)
+while True:
+ data=ser.readline().decode('utf-8').rstrip()
+ if data:
+    
+    humidity,temperature=data.split(",")
+    print(f"humedad:{humidity}%, temperatura: {temperature}°C")
+    time.sleep(1)
+
+    #rstrip: elimina espacios en blanco
+ 
